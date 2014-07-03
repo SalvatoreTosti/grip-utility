174c6f6ddb0e8a2c2cfee4a04a30062bc67005d6
Salvatore Tosti <saltosti@gmail.com>
2014-07-03 15:20:15 -0700

    Added startr command
    Added support for creating repositories with startr command.

--- grip.sh
+++ grip.sh
@@ -17,7 +17,22 @@
 
 #grip new file.txt	//makes a new file called 'file.txt' in prefered editor
 #					//then git adds 'file.txt'
-#
+
+#grip startr (filename)	//makes a new file and starts a new git repository.
+#						//default will make a file called README and open in prefered text editor
+
+
+
+#grip startb name	
+
+#grip me
+
+#grip setme	
+
+#grip syncr name
+
+
+
 
 function help_func() { #named as such to prevent overlap with default 'help' in unix
 	#statements
@@ -35,6 +50,39 @@ function new() {
 	fi
 }
 
+function startr_tell_github() {
+	NAME=$1
+	token=""
+	username="SalvatoreTosti"
+	url="https://api.github.com/user/repos"
+	
+	name="\"name\""
+	arg1="\"$NAME\""
+	
+	op1="$name: $arg1"
+	
+	autoinit="\"auto_init\""
+	arg2="true"
+	op2="$autoinit: $arg2"
+	
+	private="\"private\""
+	arg3="false"
+	op3="$private: $arg3"
+	
+	curl -i -H 'Authorization: token '"$token"'' -d '{ '"$op1"', '"$op2"', '"$op3"' }' $url
+	
+	
+	#curl -i -H 'Authorization: token d30e3e152ab490f38d96aa4b08eff9f4581329cf' -d '{ "name": "test", "auto_init": true, 	"private": false }' https://api.github.com/user/repos
+}
+
+function startr() {
+	if [ $1 ]; then
+		startr_tell_github "$1"
+	else
+		echo "Please enter a name for the new repository."
+	fi
+}
+
 
 case "$1" in
 	new)
@@ -44,9 +92,14 @@ case "$1" in
 	
 	echo "sync"
 	;;
+	
 	save)
 	
 	echo "save"
+	;;
+	startr)
+	startr "$2"
+	
 	;;
 	*)
 	echo "other"
